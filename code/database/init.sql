548e2e6600920c6e84dc899e11deadd9785c2b5b
amcm329 <amcm329@hotmail.com>
2024-05-26 12:21:43 -0600

    Update init.sql
    Modifying script because of some unknown format issue.

--- code/database/init.sql
+++ code/database/init.sql
@@ -1,38 +1,52 @@
 -- Creating and using the database. 
-CREATE DATABASE globant;
+CREATE DATABASE IF NOT EXISTS globant;
 USE globant;
-
-
-/*
-id (INTEGER) - Id of the department
-department (STRING) - Name of the departmen
-*/
-CREATE TABLE departments (
-  id INTEGER,
-  department VARCHAR(50)
-);
-
-
-/*
-id (INTEGER) - Id of the job
-job (STRING) - Name of the job
-*/
-CREATE TABLE jobs (
- id INTEGER,
- job VARCHAR(50)
-);
-
-
+      
 /*
 id (INTEGER)  - Id of the employee
 name (STRING) -  Name and surname of the employee
 datetime (STRING) -  Hire datetime in ISO format
 department_id (INTEGER) - Id of the department which the employee was hired for job_id INTEGER Id of the job which the employee was hired for
-*/
-CREATE TABLE hired_employees (
-  id INTEGER,
-  name VARCHAR(80),
-  datetime VARCHAR(50),
-  department_id INTEGER
+*/	  
+CREATE TABLE IF NOT EXISTS hired_employees (
+  Id INTEGER,
+  Name VARCHAR(80),
+  My_Datetime VARCHAR(50),
+  Department_Id INTEGER
 );
 
+INSERT INTO hired_employees
+  (Id, Name, My_Datetime, Department_Id)
+VALUES
+  (9998, 'Aaron','today',3),
+  (9999, 'Martin','today',4);
+  
+/*
+id (INTEGER) - Id of the department
+department (STRING) - Name of the departmen
+*/
+CREATE TABLE IF NOT EXISTS departments (
+  Id INTEGER,
+  Department VARCHAR(50)
+);
+
+INSERT INTO departments
+  (Id, Department)
+VALUES
+  (9998, 'Health'),
+  (9999, 'Finance');
+  
+/*
+id (INTEGER) - Id of the job
+job (STRING) - Name of the job
+*/
+CREATE TABLE IF NOT EXISTS jobs (
+ Id INTEGER,
+ Job VARCHAR(50)
+);
+
+INSERT INTO jobs
+  (Id, Job)
+VALUES
+  (9998, 'Programmer'),
+  (9999, 'Accountant');
