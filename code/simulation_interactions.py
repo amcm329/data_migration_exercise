45b5ec531c3dd7f410e929a4c0f89fc387356fed
amcm329 <amcm329@hotmail.com>
2024-05-26 18:30:21 -0600

    Update simulation_interactions.py
    Implemented functions backup and get_backup_names, this last one is an extra to figure out which backups are stored.

--- code/simulation_interactions.py
+++ code/simulation_interactions.py
@@ -4,39 +4,91 @@
 """
 
 import requests 
-import datetime
 
+#Creating the headers section for all the following requests. 
 headers = {
             'Content-type':'application/json', 
             'Accept':'application/json'
           }
+
     
-    
-def backup_table(table = "hired_employees"): 
+"""
+   This is a function meant to test the creation of backups, according to the exercise statements. 
+   
+   Parameters:
+     table_name - the table name to be backed up. 
+   
+   Returns: 
+    A dictionary with the following structure:
+      table - the table name.
+      status - HTTP status code 
+      message - a more particular message depending on the situations during the process. 
+      count - the number of rows that contains the avro file.       
+      mimetype - the document type.
+"""    
+def backup_table(table_name): 
 
     additional_data = { 
-                        'table_name': table, 
+                        'table_name': table_name, 
                       }
                       
     response = requests.post('http://127.0.0.1:5000/backup', json=additional_data, headers=headers)
-    
-    #response = requests.get('http://127.0.0.1:5000', params=additional_data)
-    
-    
-    print(response.json())
+        
+    return response.json()
 
 
+"""
+   This function retrieves all available backups.
+   
+   Returns: 
+    A dictionary with the following structure:
+      files - all files names.
+      
+   Important: 
+     if the list is empty, it means that there are no available backups.
+"""
+def get_backup_names():
+                      
+    response = requests.get('http://127.0.0.1:5000/backup_names',  headers=headers)
+        
+    return response.json()
 
-def restore_table(): 
+
+"""
+   This is a function meant to test the creation of backups, according to the exercise statements. 
+   
+   Parameters:
+     table_name - the table name to be backed up. 
+   
+   Returns: 
+    A dictionary with the following structure:
+      table - the table name.
+      status - HTTP status code 
+      message - a more particular message depending on the situations during the process. 
+      count - the number of rows that contains the avro file.       
+      mimetype - the document type.
+"""    
+def restore_table(table, timestamp = None): 
+    
     pass
     
 
 
+def insert_table(filename, csv_path = "/home/sources/", number_of_rows = 1000): 
+    pass 
 
-def get_current_date(): 
-     #datetime.datetime.now().isoformat()
-    pass
 
-print(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
 
-#backup_table()
+"""
+   This is the section related to the tests. 
+"""
+
+# **** BACKUP *****
+#print(backup_table("hired_employees"))
+#print(backup_table("jobs"))
+#print(backup_table("departments"))
+#print(backup("other")) 
+
+print(get_backup_names())
+
+# **** RESTORE *****
